8ec7f2118a2a29c2d58d8aa2a78a7cdc83d92c77
mubarak23 <mubarakaminu340@gmail.com>
2022-06-22 06:58:58 +0100

    valid Anagram question

--- index.js
+++ index.js
@@ -18,3 +18,64 @@ const containsDuplicate = (nums) => {
 // Runtime: 153 ms, faster than 35.60% of JavaScript online submissions for Contains Duplicate.
 // Memory Usage: 51.1 MB, less than 40.19% of JavaScript online submissions for Contains Duplicate.
 // Next challenges:
+
+// Question 2: 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different 
+// word or phrase, typically using all the original letters exactly once.
+
+
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+const isAnagram = (s,t) => {
+    let map = {}
+    if(s.length !== t.length){
+        return false
+    }
+
+    for(i = 0; i < s.length; i++){
+        if(map[s[i]]){
+            map[s[i]]++
+        }else{
+            map[s[i]] = 1
+        }
+    }
+    for(i = 0; i <t.length; t++){
+        if(map[t[i]]){
+            map[t[i]]--
+        }else{
+            return false
+        }
+    }
+
+    return true
+
+}
+
+
+// accepted solution
+const isAnagramT = (s, t) => {
+    let len = s.length, fMap = new Int16Array(123)
+    if(t.length !== len) return false
+    for(let i = 0; i < len; i++){
+        fMap[s.charCodeAt(i)]++
+    }
+    
+    for(let i = 0; i < len; i++){
+        if (--fMap[t.charCodeAt(i)] < 0) return false
+    }
+    return true
+}
+
+// Runtime: 117 ms, faster than 51.04% of JavaScript online submissions for Valid Anagram.
+// Memory Usage: 41.9 MB, less than 99.65% of JavaScript online submissions for Valid Anagram.
+// Next challenges:
