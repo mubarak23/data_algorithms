3b61aca8fe42b643c1f2ea0500a94728fcf9589f
mubarak23 <mubarakaminu340@gmail.com>
2022-06-24 08:01:22 +0100

    2 sum I and II

--- index.js
+++ index.js
@@ -79,3 +79,47 @@ const isAnagramT = (s, t) => {
 // Runtime: 117 ms, faster than 51.04% of JavaScript online submissions for Valid Anagram.
 // Memory Usage: 41.9 MB, less than 99.65% of JavaScript online submissions for Valid Anagram.
 // Next challenges:
+
+
+
+// 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+const twoSum = (nums, target) => {
+    let map = {}
+    for(let i = 0; i <nums.length; i++){
+        if(target - nums[i] in map){
+            return [map[target - nums[i]], i]
+        }else{
+            map[nums[i]] = i
+        }
+    }
+}
+
+
+
+// 167. Two Sum II - Input Array Is Sorted
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+// find two numbers such that they add up to a specific target number. 
+// Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+const twoSum2 = (numbers, target) => {
+    let start = 0
+    let end = numbers.length - 1
+    while(start < end){
+        let currSum = numbers[start] + numbers[end]
+        if(currSum > target) end--
+        else if(currSum < target) start++
+        else return [start + 1, end + 1]
+    }
+    return []
+}
+
+// Runtime: 93 ms, faster than 48.18% of JavaScript online submissions for Two Sum II - Input Array Is Sorted.
+// Memory Usage: 42.6 MB, less than 86.53% of JavaScript online submissions for Two Sum II - Input Array Is Sorted.
+// Next challenges:
